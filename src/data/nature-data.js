8d15123a78dd5234271b8fe1255839bbea79e51c
karolinfrennert <karolinfrennert@gmail.com>
2021-02-25 12:12:45 +0100

    Added heading in db

--- src/data/nature-data.js
+++ src/data/nature-data.js
@@ -1,6 +1,7 @@
 export const natureQuestions = [
   {
     id: 1,
+    heading: "First question",
     question:
       "How much plastic do you think the avarage person consumes in a month?",
     correct_answer: "1",
@@ -8,6 +9,7 @@ export const natureQuestions = [
   },
   {
     id: 2,
+    heading: "second question",
     question: "How much Co2 is released into the atmosphere everywhere?",
     correct_answer: "1",
     incorrect_answer: ["2", "3"],
