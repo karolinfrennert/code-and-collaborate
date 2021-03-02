d47f7483b28da55da5d114050aa89bdd5692c69e
ebbaj <ebba-jonsson@hotmail.com>
2021-03-02 15:40:32 +0100

    Added fadeOut and fadeIn classes

--- src/main.js
+++ src/main.js
@@ -5,3 +5,18 @@ natureQuestions.forEach((e) => {
 
   console.log(id, question, correct_answer, incorrect_answer);
 });
+
+const startButton = document.querySelector('.start-button');
+const introPage = document.querySelector('.intro-page');
+const categoryIntro = document.querySelector('.category-intro');
+const quizIntro = document.querySelector('.quiz-intro');
+
+const toggle = () => {
+  introPage.classList.add('fadeOut');
+  categoryIntro.classList.add('fadeIn');
+  quizIntro.classList.add('fadeIn');
+}
+
+startButton.addEventListener('click', toggle);
+
+
