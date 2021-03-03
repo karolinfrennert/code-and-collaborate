71d5e083243f63620269efe9afaeea6d8b66e64c
karolinfrennert <karolinfrennert@gmail.com>
2021-03-03 11:08:04 +0100

    Cleanup

--- src/main.js
+++ src/main.js
@@ -1,22 +1,12 @@
-import { natureQuestions } from "./data/nature-data.js";
-
-natureQuestions.forEach((e) => {
-  const { id, question, correct_answer, incorrect_answer } = e;
-
-  console.log(id, question, correct_answer, incorrect_answer);
-});
-
-const startButton = document.querySelector('.start-button');
-const introPage = document.querySelector('.intro-page');
-const categoryIntro = document.querySelector('.category-intro');
-const quizIntro = document.querySelector('.quiz-intro');
+const startButton = document.querySelector(".start-button");
+const introPage = document.querySelector(".intro-page");
+const categoryIntro = document.querySelector(".category-intro");
+const quizIntro = document.querySelector(".quiz-intro");
 
 const toggle = () => {
-  introPage.classList.add('fadeOut');
-  categoryIntro.classList.add('fadeIn');
-  quizIntro.classList.add('fadeIn');
-}
+  introPage.classList.add("fadeOut");
+  categoryIntro.classList.add("fadeIn");
+  quizIntro.classList.add("fadeIn");
+};
 
-startButton.addEventListener('click', toggle);
-
-
+startButton.addEventListener("click", toggle);
