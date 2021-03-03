1d0aa69f5e7024df1f51775565dde7a5e8ea138c
karolinfrennert <karolinfrennert@gmail.com>
2021-03-03 11:40:49 +0100

    button event listener

--- src/quiz.js
+++ src/quiz.js
@@ -1,7 +1,21 @@
 import { natureQuestions } from "./data/nature-data.js";
 
+const button1 = document.querySelector(".button-1");
+const button2 = document.querySelector(".button-2");
+const button3 = document.querySelector(".button-3");
+const button4 = document.querySelector(".button-4");
+const button5 = document.querySelector(".button-5");
+const button6 = document.querySelector(".button-6");
+const button7 = document.querySelector(".button-7");
+
+console.log(button1);
+
+const alert = () => {
+  console.log("hey hey hey");
+};
+
+button1.addEventListener("click", alert);
+
 natureQuestions.forEach((e) => {
   const { id, question, correct_answer, incorrect_answer } = e;
-
-  console.log(id, question, correct_answer, incorrect_answer);
 });
