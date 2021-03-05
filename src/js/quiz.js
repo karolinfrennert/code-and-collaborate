f6ccbf476ad04b2da1dc4770b592a6498ed77a2c
ebbaj <ebba-jonsson@hotmail.com>
2021-03-05 15:26:06 +0100

    Added code for modal in html + css + js

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,13 +1,32 @@
 import { natureQuestions } from "../data/nature-data.js";
 
-const button1 = document.querySelector(".button-1");
+/* const button1 = document.querySelector(".button-1");
 const button2 = document.querySelector(".button-2");
 const button3 = document.querySelector(".button-3");
 const button4 = document.querySelector(".button-4");
 const button5 = document.querySelector(".button-5");
 const button6 = document.querySelector(".button-6");
-const button7 = document.querySelector(".button-7");
-const mainWrapper = document.querySelector("main");
+const button7 = document.querySelector(".button-7"); */
+const buttons = document.querySelectorAll(".button")
+const modal = document.querySelector(".modal");
+var close = document.querySelector(".close");
+const flexWrapper = document.querySelector('.flex-wrapper');
+
+const openModal = () => modal.style.display = "block";
+
+buttons.forEach((button) => {
+  button.addEventListener('click', openModal);
+})
+
+close.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
 
 natureQuestions.forEach((data) => {
   const { id, heading, question, correct_answer, incorrect_answer } = data;
@@ -17,8 +36,9 @@ natureQuestions.forEach((data) => {
   const correctAnswer = document.createElement('button');
   const incorrectAnswer1 = document.createElement('button');
   const incorrectAnswer2 = document.createElement('button');
-
+  
   cardWrapper.className = "card-wrapper";
+  cardWrapper.id = id;
 
   cardTitle.innerText = heading;
   cardWrapper.appendChild(cardTitle);
@@ -35,9 +55,9 @@ natureQuestions.forEach((data) => {
   incorrectAnswer2.innerText = incorrect_answer[1];
   cardWrapper.appendChild(incorrectAnswer2);
 
+  flexWrapper.appendChild(cardWrapper);
 
   console.log(cardWrapper);
 
-
-
 });
+
