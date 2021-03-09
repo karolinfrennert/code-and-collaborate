b03d0ae1fe110a00d84ce9f16ade5d63b0233b17
karolinfrennert <karolinfrennert@gmail.com>
2021-03-09 11:36:52 +0100

    Question card works

--- src/data/nature-data.js
+++ src/data/nature-data.js
@@ -9,7 +9,44 @@ export const natureQuestions = [
   },
   {
     id: 2,
-    heading: "second question",
+    heading: "Second question",
+    question: "How much Co2 is released into the atmosphere everywhere?",
+    correct_answer: "1",
+    incorrect_answer: ["2", "3"],
+  },
+  {
+    id: 3,
+    heading: "Third question",
+    question:
+      "How much plastic do you think the avarage person consumes in a month?",
+    correct_answer: "1",
+    incorrect_answer: ["2", "3"],
+  },
+  {
+    id: 4,
+    heading: "Fourth question",
+    question: "How much Co2 is released into the atmosphere everywhere?",
+    correct_answer: "1",
+    incorrect_answer: ["2", "3"],
+  },
+  {
+    id: 5,
+    heading: "Fifth question",
+    question:
+      "How much plastic do you think the avarage person consumes in a month?",
+    correct_answer: "1",
+    incorrect_answer: ["2", "3"],
+  },
+  {
+    id: 6,
+    heading: "Sixth question",
+    question: "How much Co2 is released into the atmosphere everywhere?",
+    correct_answer: "1",
+    incorrect_answer: ["2", "3"],
+  },
+  {
+    id: 7,
+    heading: "Seventh question",
     question: "How much Co2 is released into the atmosphere everywhere?",
     correct_answer: "1",
     incorrect_answer: ["2", "3"],
--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,63 +1,62 @@
 import { natureQuestions } from "../data/nature-data.js";
 
-/* const button1 = document.querySelector(".button-1");
-const button2 = document.querySelector(".button-2");
-const button3 = document.querySelector(".button-3");
-const button4 = document.querySelector(".button-4");
-const button5 = document.querySelector(".button-5");
-const button6 = document.querySelector(".button-6");
-const button7 = document.querySelector(".button-7"); */
-const buttons = document.querySelectorAll(".button")
+const buttons = document.querySelectorAll(".button");
 const modal = document.querySelector(".modal");
 var close = document.querySelector(".close");
-const flexWrapper = document.querySelector('.flex-wrapper');
+const flexWrapper = document.querySelector(".flex-wrapper");
 
-const openModal = () => modal.style.display = "block";
+let nature = natureQuestions.map(
+  ({ id, heading, question, correct_answer, incorrect_answer }) => {
+    const cardWrapper = document.createElement("div");
+    const cardTitle = document.createElement("h1");
+    const questionParagraph = document.createElement("p");
+    const correctAnswer = document.createElement("button");
+    const incorrectAnswer1 = document.createElement("button");
+    const incorrectAnswer2 = document.createElement("button");
+
+    cardWrapper.className = "card-wrapper";
+    cardWrapper.id = id;
+
+    cardTitle.innerText = heading;
+    cardWrapper.appendChild(cardTitle);
+
+    questionParagraph.innerText = question;
+    cardWrapper.appendChild(questionParagraph);
+
+    correctAnswer.innerText = correct_answer;
+    cardWrapper.appendChild(correctAnswer);
+
+    incorrectAnswer1.innerText = incorrect_answer[0];
+    cardWrapper.appendChild(incorrectAnswer1);
+
+    incorrectAnswer2.innerText = incorrect_answer[1];
+    cardWrapper.appendChild(incorrectAnswer2);
+
+    return cardWrapper;
+  }
+);
+
+const openModal = () => {
+  flexWrapper.innerHTML = "";
+  modal.style.display = "block";
+};
+
+function loopButton() {
+  const id = this.id;
+  flexWrapper.appendChild(nature[id - 1]);
+}
+
+close.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
 buttons.forEach((button) => {
-  button.addEventListener('click', openModal);
-})
-
-close.onclick = function() {
-    modal.style.display = "none";
-};
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-
-natureQuestions.forEach((data) => {
-  const { id, heading, question, correct_answer, incorrect_answer } = data;
-  const cardWrapper = document.createElement('div');
-  const cardTitle = document.createElement('h1');
-  const questionParagraph = document.createElement('p');
-  const correctAnswer = document.createElement('button');
-  const incorrectAnswer1 = document.createElement('button');
-  const incorrectAnswer2 = document.createElement('button');
-  
-  cardWrapper.className = "card-wrapper";
-  cardWrapper.id = id;
-
-  cardTitle.innerText = heading;
-  cardWrapper.appendChild(cardTitle);
-
-  questionParagraph.innerText = question;
-  cardWrapper.appendChild(questionParagraph);
-
-  correctAnswer.innerText = correct_answer;
-  cardWrapper.appendChild(correctAnswer);
-
-  incorrectAnswer1.innerText = incorrect_answer[0];
-  cardWrapper.appendChild(incorrectAnswer1);
-
-  incorrectAnswer2.innerText = incorrect_answer[1];
-  cardWrapper.appendChild(incorrectAnswer2);
-
-  flexWrapper.appendChild(cardWrapper);
-
-  console.log(cardWrapper);
-
+  button.addEventListener("click", openModal);
+  button.addEventListener("click", loopButton);
 });
-
