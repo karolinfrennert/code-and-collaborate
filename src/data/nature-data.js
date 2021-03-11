ebc0ce327bd61d8f0b130d495c6d11a12ff67613
ebbaj <ebba-jonsson@hotmail.com>
2021-03-11 15:18:30 +0100

    Started changing quiz questions

--- src/data/nature-data.js
+++ src/data/nature-data.js
@@ -3,47 +3,47 @@ export const natureQuestions = [
     id: 1,
     heading: "First question",
     question:
-      "How much plastic do you think the avarage person consumes in a month?",
-    correct_answer: "1",
-    incorrect_answer: ["2", "3"],
+      "How many years does it take a single aluminium can to decompose?",
+    correct_answer: "80-100 years",
+    incorrect_answer: ["20-40 years", "60-80 years"],
     correct_prompt:
-      "Correct! Sadly it takes between 80-100 years for a single aluminium can to decompose. At Ricola we have as a target to reduce waste by 7 % (50 tonnes) for 2021.",
+      "Correct! Sadly it takes between 80-100 years for a single aluminium can to decompose. \n \n At Ricola we have as a target to reduce waste by 7 % (50 tonnes) for 2021.",
     incorrect_prompt:
-      "Damn it, you’re close! Actually it’s about 8.8 million tons. At Ricola we try our best to reduce the plastic in our packaging and experimenting using waste material from our production. Did you know? The amount of plastic dumped in our oceans is equivalent to one garbage truck every minute.",
+      "Damn it, you’re close! Actually it’s about 80-100 years. \n \n At Ricola we try our best to reduce the plastic in our packaging and experimenting using waste material from our production. \n \n Did you know? The amount of plastic dumped in our oceans is equivalent to one garbage truck every minute.",
   },
   {
     id: 2,
     heading: "Second question",
-    question: "How much Co2 is released into the atmosphere everywhere?",
-    correct_answer: "1",
-    incorrect_answer: ["2", "3"],
+    question: "If you recycle a tonne of paper, how many trees are you saving?",
+    correct_answer: "17",
+    incorrect_answer: ["12", "23"],
     correct_prompt:
-      " HELLO Correct! Sadly it takes between 80-100 years for a single aluminium can to decompose. At Ricola we have as a target to reduce waste by 7 % (50 tonnes) for 2021.",
+      "Correct! It’s about 8.8 million tons. \n \n At Ricola we try our best to reduce the plastic in our packaging and experimenting using waste material from our production. \n \n Did you know? The amount of plastic dumped in our oceans is equivalent to one garbage truck every minute.",
     incorrect_prompt:
-      "Damn it, you’re close! Actually it’s about 8.8 million tons. At Ricola we try our best to reduce the plastic in our packaging and experimenting using waste material from our production. Did you know? The amount of plastic dumped in our oceans is equivalent to one garbage truck every minute.",
+      "Damn it, you’re close! Actually it’s about 8.8 million tons. \n \n At Ricola we try our best to reduce the plastic in our packaging and experimenting using waste material from our production. \n \n Did you know? The amount of plastic dumped in our oceans is equivalent to one garbage truck every minute.",
   },
   {
     id: 3,
     heading: "Third question",
     question:
-      "How much plastic do you think the avarage person consumes in a month?",
-    correct_answer: "1",
-    incorrect_answer: ["2", "3"],
+      "About 71% of the Earth is covered with water. How much of it is fresh water?",
+    correct_answer: "2.5%",
+    incorrect_answer: ["11%", "15%"],
     correct_prompt:
-      "Correct! Sadly it takes between 80-100 years for a single aluminium can to decompose. At Ricola we have as a target to reduce waste by 7 % (50 tonnes) for 2021.",
+      "Correct! \n \n Did you know? A five-minute shower uses about 40 litres of water. That’s about half the volume of water used when taking a bath.",
     incorrect_prompt:
-      "Damn it, you’re close! Actually it’s about 8.8 million tons. At Ricola we try our best to reduce the plastic in our packaging and experimenting using waste material from our production. Did you know? The amount of plastic dumped in our oceans is equivalent to one garbage truck every minute.",
+      "Damn it, you're close! \n \n Did you know? A five-minute shower uses about 40 litres of water. That’s about half the volume of water used when taking a bath.",
   },
   {
     id: 4,
     heading: "Fourth question",
-    question: "How much Co2 is released into the atmosphere everywhere?",
-    correct_answer: "1",
-    incorrect_answer: ["2", "3"],
+    question: "What percent of food never gets eaten worldwide?",
+    correct_answer: "30%",
+    incorrect_answer: ["50%", "10%"],
     correct_prompt:
-      "Correct! Sadly it takes between 80-100 years for a single aluminium can to decompose. At Ricola we have as a target to reduce waste by 7 % (50 tonnes) for 2021.",
+      "<h1>Correct!</h1><p>30% of food never gets eaten worldwide. At Ricola we</p>",
     incorrect_prompt:
-      "Damn it, you’re close! Actually it’s about 8.8 million tons. At Ricola we try our best to reduce the plastic in our packaging and experimenting using waste material from our production. Did you know? The amount of plastic dumped in our oceans is equivalent to one garbage truck every minute.",
+      "<h1>Incorrect.</h1><p>Actually it’s about 30%. At Ricola we</p>",
   },
   {
     id: 5,
